f3a4b84a81723cec255d100e254546dd31b8a021
Peyyala Venkata Karthik <peyyalavenkatakarthik4@gmail.com>
2026-07-23 13:19:00 +0530

    updat api

--- src/api/api.js
+++ src/api/api.js
@@ -4,4 +4,19 @@ const API = axios.create({
   baseURL: "https://ai-resume-screening-backend-i5ki.onrender.com/api",
 });
 
+// Attach JWT token to every request
+API.interceptors.request.use(
+  (config) => {
+
+    const token = localStorage.getItem("token");
+
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+
+    return config;
+  },
+  (error) => Promise.reject(error)
+);
+
 export default API;
